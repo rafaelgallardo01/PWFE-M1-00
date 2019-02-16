95a2bbd720745733a78fca5b56f885c225071f6e
rafaelgallardo01 <rafaelgallardo@hotmail.com>
2019-02-16 01:34:47 -0300

    estudiando

---
+++ texto_rafa_.php
@@ -0,0 +1,59 @@
+<HTML:5>
+
+
+
+ide Arduino 
+
+sudo chmod +x install.sh
+
+./install.sh    ./install.shinstall.sh
+
+
+
+----------------------------------
+  sudo
+apt-get update
+apt-get upgrade
+apt-get install git
+
+
+$ git config --global user.name "rafaelgallardo01"
+
+$ git config --global user.email rafaelgallardo@hotmail.com
+
+git config --global core.editor "code --wait"
+
+ssh-keygen -t rsa -b 4096 -C " rafaelgallardo@hotmail.com "
+
+presione Entrar.
+contraseña segura
+
+ git help 
+---------------------------------------------
+puertos com 
+
+sudo usermod -a -G dialout rafael
+ext install MS-CEINTL.vscode-language-pack-es
+sudo chmod 777 /dev/ttyUSB0
+
+/home/rafael/.arduino15/
+------------------
+visual studio  español
+ext install MS-CEINTL.vscode-language-pack-es
+_--------------------------------
+
+coamandos Arduino
+
+Arduino: Board Manager: Seleccionar la tarjeta del proyecto actual
+Arduino: Upload: Compilar y subir sketch (Control + Alt + U)
+Arduino: Verify: Compilar sketch (Control + Alt + R)
+Arduino: Examples: Mostrar lista de ejemplos
+Arduino: Library Manager: Explorar y gestionar librerías
+Arduino: Open Serial Monitor: Abrir el puerto serie
+Arduino: Close Serial Monitor: Detener el puerto de serie
+Arduino: Select Serial Port: Cambiar el puerto de serie
+Arduino: Change Baud Rate: Cambiar el baud rate del puerto Serie
+Arduino: Change Board Type: Cambiar el tipo de tarjeta
+Arduino: Send Text to Serial Port: Ennviar la linea seleccionada por puerto serie
+
+</HTML:5>
