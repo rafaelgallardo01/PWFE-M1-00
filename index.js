1132f001ddf48d6f0c93aecbfe6538d2a22a09a0
rafaelgallardo01 <rafaelgallardo@hotmail.com>
2019-03-23 10:02:24 -0300

    estudiando

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const http = require('http')
-const fs = require('fs')
-const path = require ('path') 
+const http = require("http")
+const fs = require("fs")
+const path = require("path")
 
 const port = 80
 
 http.createServer(function(request, response){
-	let dir = 'public/' //<--Carpeta del proyecto
+	let dir = "public/" //<-- Carpeta del proyecto
 
-	let file = (request.url == '/') ? 'index.html' : request.url //<-- Archivo solicitado
-	let ext = String( path.extname(file)).toLowerCase() //<-- extenciones html,css,js,etc
+	//let file = request.url //<-- Archivo solicitado
+	/*
+		if( request.url == "/" ){
+			let file = "index.html"
+		} else {
+			let file = request.url
+		}
+	*/
+	let file = (request.url == "/") ? "index.html" : request.url //<-- Archivo solicitado
+	let ext = String( path.extname(file) ).toLowerCase() //<-- extensiones ".html", ".css", ".js", etc
 
 	let tipos = {
 			".html"	: "text/html",
@@ -27,21 +35,21 @@ http.createServer(function(request, response){
 			".eot"	: "application/vnd.ms-fontobject",
 			".otf"	: "application/font-otf",
 			".svg"	: "application/image/svg+xml"
-}
+	}
 
-	let contenType = tipos[ext]  || 'application/octet-stream'
+	let contenType = tipos[ext] || "application/octet-stream"
 
-	console.log('Archivo solicitado: ' + dir + file)
+	console.log("Archivo solicitado: " + dir + file)
 
-    fs.readFile(dir + file, function(error, content){ //<-- 'intentar leer el recurso solicitado'
+	fs.readFile(dir + file, function(error, content){ //<-- "Intentar" leer el recurso solicitado
 
-    	if( error ){ //<-- si hay un error...
-    		response.end('Archivo no encontrado :(')
-    	} else { //<-- si lo encontro ...
-    		response.writeHead(200, { 'Content-Type' : contenType})
-    		response.end(content)
-    	}
+		if( error ){ //<-- Si hay un error...
+			response.end("Archivo no encontrado :(")
+		} else { //<-- Si lo encontro...
+			response.writeHead(200, { "Content-Type" : contenType })
+			response.end(content)
+		}
 
-    })
+	})
 
 }).listen(port)
